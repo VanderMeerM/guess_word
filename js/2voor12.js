c6f2421a04d98421ea38300e4371623d8d4a0e67
Marcel van der Meer <marcelmeer@gmail.com>
2024-10-29 15:17:21 +0100

    added functionality and words

--- js/2voor12.js
+++ js/2voor12.js
@@ -1,5 +1,8 @@
 
     const randomIndexOrgWord = Math.round(Math.random() * originalWords.length);
+    const congrats =  document.getElementById('congrats');
+    const time =  document.getElementById('time');
+
     let chosenWord = originalWords[randomIndexOrgWord];
 
    let arrayOfChosenWord = [];
@@ -38,30 +41,73 @@ function getIDX(num) {
    document.querySelector('.word_hussled').children[num].innerHTML = '';
    }
 
-   
+//rij rode nummers 
 for (let i=1; i<=chosenWord.length; i++) {
     document.querySelector('.word_numbers').innerHTML += 
     `<div class="numbers">${i} </div>`;
 }
 
+// rij letterkaders 
 for (let i=0; i<chosenWord.length; i++) {
     document.querySelector('.word_solution').innerHTML += 
     `<div class="letterbox"> </div>`;
 }
 
+function showSolution(color, message) {
+for (let i=0; i< hussledWord.length; i++) {
+
+    document.querySelector('.word_solution').children[hussledWord[i].idx].innerHTML = 
+   (hussledWord[i].cw).toUpperCase();
+
+   document.querySelector('.word_hussled').children[i].innerHTML = '';
+   congrats.style.color = color;
+   congrats.textContent = message;
+   document.getElementById('reload').style.visibility = 'visible';
+}
+}
+
 document.getElementById('checkWord').addEventListener('click', () => {
     if (document.getElementById('guessed_word').value !='') {
+
         if (document.getElementById('guessed_word').value === chosenWord) {
-            console.log('GOED!');
+
+        clearInterval(timeRuns);
+        showSolution('green', 'Gefeliciteerd! Je hebt het woord geraden!');
+                       
         } else {
-            console.log('Helaas...');
-        }
+            clearInterval(timeRuns);
+            showSolution('red', `Helaas! De oplossing was ${chosenWord}...`);
+       }
+       
     };
 })
 
 document.getElementById('reload').addEventListener('click', () => {
     window.location.reload();
 })
+
+// timer
+
+   timeInRound = 119;
+
+   time.innerHTML = `2:00`;
+
+        let timeRuns = setInterval(() => {
+            let minutes = Math.floor(timeInRound / 60);
+            let seconds = timeInRound - (minutes * 60);
+
+            (seconds >= 10) ?
+                time.innerHTML = `${minutes}:${seconds}` :
+                time.innerHTML = `${minutes}:0${seconds}`
+
+            if (timeInRound > 0) { timeInRound-- }
+
+            else {
+                clearInterval(timeRuns);
+                showSolution('red', `Helaas! De tijd is om...het antwoord was ${chosenWord}...`);
+            }
+        }, 1000)
+    
 
 console.log(chosenWord)
 
