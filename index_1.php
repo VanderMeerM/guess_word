f9992bf8bbab66b7170b3473d3ca2f42d14fb422
Marcel van der Meer <marcelmeer@gmail.com>
2025-11-07 22:10:08 +0100

    changed manifest

--- index_1.php
+++ index_1.php
@@ -7,7 +7,7 @@
     <script type="text/javascript" src="./js/12_letter_words.js"></script>
     <script defer type="text/javascript" src="./js/2voor12.js"></script>
     <link rel="stylesheet" type="text/css" href="./2voor12.css" />
-    <link rel="manifest" href="./js/manifest.json">
+    <link rel="manifest" href="manifest.json">
     <link rel="icon" type="image/x-icon" href="./2voor12/img/favicon.ico">
 </head>
 
